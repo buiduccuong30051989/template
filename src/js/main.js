54f9996a31660ce98970eea0ad18fac056bb1ce5
cuongbd-0793 <bui.duc.cuong@sun-asterisk.com>
2021-05-05 15:25:24 +0700

    [WIP][Component][Button] Simple Button Variant

--- src/js/main.js
+++ src/js/main.js
@@ -10,15 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
     root.classList.add('dark');
   }
 
-  const btn = document.querySelector('.js-toggle-dark-mode');
-  if (btn) {
-    btn.addEventListener('click', function () {
-      root.classList.toggle('dark');
-      if (root.classList.contains('dark')) {
-        localStorage.setItem('themePreference', 'dark');
-      } else {
-        localStorage.setItem('themePreference', 'light');
-      }
+  const btns = document.querySelectorAll('.js-toggle-dark-mode');
+  if (btns.length) {
+    btns.forEach(function (currentBtn) {
+      currentBtn.addEventListener('click', function () {
+        root.classList.toggle('dark');
+        if (root.classList.contains('dark')) {
+          localStorage.setItem('themePreference', 'dark');
+        } else {
+          localStorage.setItem('themePreference', 'light');
+        }
+      });
     });
   }
 });
